79118586bc77277c59c2b9c8be31de4a4a9e6876
Stephen Ierodiaconou <stevegeek@gmail.com>
2024-04-25 12:39:07 +0200

    We only need to duplicate the collection if we will shuffle it

--- lib/hashids2.rb
+++ lib/hashids2.rb
@@ -151,15 +151,15 @@ class Hashids2
   end
 
   def consistent_shuffle(collection_to_shuffle, salt_part_1, salt_part_2, max_salt_length)
-    chars = collection_to_shuffle.dup
-
     salt_part_1_length = salt_part_1.length
 
-    return chars if collection_to_shuffle.empty? || max_salt_length == 0 || salt_part_1.nil? || salt_part_1_length == 0
+    return collection_to_shuffle if collection_to_shuffle.empty? || max_salt_length == 0 || salt_part_1.nil? || salt_part_1_length == 0
+
+    chars = collection_to_shuffle.dup
 
     idx = ord_total = 0
 
-    i = collection_to_shuffle.length-1
+    i = collection_to_shuffle.length - 1
     while i >= 1
       raise ArgumentError, "Salt is too short in shuffle" if idx >= salt_part_1_length && salt_part_2.nil?
       ord_total += n = (idx >= salt_part_1_length ? salt_part_2[idx - salt_part_1_length] : salt_part_1[idx]).ord
